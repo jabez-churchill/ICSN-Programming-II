8f15ff9b1449ef2ee8931910bdc565b3408b97ac
Aaronious <67837213+jabez-churchill@users.noreply.github.com>
2023-01-09 13:35:10 +0700

    Add files via upload
    Task for week 1, day 1.

---
+++ py2-assignment-1.py
@@ -0,0 +1,28 @@
+myString = "Hello World. My name is " # This is called an "inline comment"
+
+'''
+This is a multi-line comment block.
+Everything we write inside of the triple quotes is a comment, 
+effecting nothing in our program.
+
+Below are some typical comment tags for you to use.
+'''
+
+#_______________No Action Required________________________
+# HACK tells us that part of a code is a "bad solution", or messy, and should be improved.
+# XXX shows us something needs attention, or is important to know.
+# NOTE shows us some detail, not super important.
+# UNDONE tells us something was changed back, after a change.
+
+#_______________Action Required________________________
+# BUG  is a comment tag that shows where there is a bug.
+# FIXME shows us something is broken.
+# TODO is a comment tag that shows (others, or reminds yourself) that you need to finish something.
+# TODO(2) adds a number to the comment tag for organization... for priority scale or numbered tasks.
+
+
+#_______________YOUR ASSIGNMENT________________________
+#TODO(1) Declare a new variable myString, with a value of "My name is ".
+#TODO(2) Declare a new variable myName, with a value of "[your name]".
+#TODO(3) Declare a new function that prints out both strings on one line.
+#TODO(4) Call your function.
