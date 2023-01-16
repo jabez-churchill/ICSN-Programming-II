1496feb376acf75ae7ae7335db9b645810569421
Aaronious <67837213+jabez-churchill@users.noreply.github.com>
2023-01-16 12:56:21 +0700

    Directions for Assignment 2

--- assignment2.py
+++ assignment2.py
@@ -1,4 +1,35 @@
+# Dictionary for Python
+# TODO(1) Declare a new dictionary with the KEYS listed below, then answer them with your personal information as VALUES.
 
+#____FORMAT________
+# nickname: ____
+# homeroom: ____
+# age: ____
+# programming-language: ____
+
+myData = {
+
+}
+
+
+# Dictionary for Python
+# TODO(2) Add New Programming Terms here. All of these keys and values should be Strings, inside quotation marks ("").
+
+#____FORMAT____
+# {term: description}
+
+#___TERMS____
+# 1. dictionary
+# 2. list
+# 3. function
+# 4. variable 
+
+
+# TODO(3) Retrieve the value of Key item "dictionary".
+
+
+
+# TODO(4) Make your own example of a dictionary.
 
 
 
