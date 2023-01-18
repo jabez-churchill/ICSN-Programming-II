da72274886efbb20e19782f9a0af3f5bcec96476
Aaron <aaroniousdesigns@gmail.com>
2023-01-18 14:44:27 +0700

    outline

---
+++ function-challenge01.py
@@ -0,0 +1,16 @@
+import random
+
+# TODO(1) Lists
+brandList = [""]
+
+# TODO(2) Generate Random Int to use as List Index
+def GetRandomInt():
+    randomInt = random.randint(0,10) # GetFromRandomRange
+    return randomInt
+
+# TODO(3) Define function that uses our random int as an index in each list, and prints results.
+def GenerateRandomCar():
+    print("Yay")
+
+
+GenerateRandomCar()
