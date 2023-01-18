b712d7614cc8372aa00ba5d4fb4efe76773f105d
Aaron <aaroniousdesigns@gmail.com>
2023-01-18 21:35:17 +0700

    for W3D1, function challenge part two

--- function-challenge01.py
+++ function-challenge01.py
@@ -1,16 +1,52 @@
 import random
 
-# TODO(1) Lists
-brandList = [""]
+# DONE(1) Lists
+brandList = [
+        "Acura",
+        "Honda",
+        "BMW",
+        "Ferrari",
+        "Ford",
+        "Toyota",
+        "Audi",
+        "Dodge",
+        "Cadillac",
+        "Chevy"
+        ]
 
-# TODO(2) Generate Random Int to use as List Index
-def GetRandomInt():
-    randomInt = random.randint(0,10) # GetFromRandomRange
+doorNumberList = [2, 4, 5]
+wheelDriveList = [2, 4]
+engineSizeList = ["1L", "1.6L", "2L", "3L"]
+
+defectList = [
+        "that only drives in reverse.",
+        "with heavy fire damage.",
+        "that occasionally explodes.",
+        "modified to use only three wheels.",
+        "made of 100% beef."
+        ]
+
+# TODO(4) a dictionary of lists (as values).
+
+
+
+# DONE(2) generate Random Int to use as List Index
+def GetRandomInt(listSize):
+    # randomInt = random.randint(0,10) # HOW DO WE MAKE THIS ADAPT TO DIFFERENT SIZE LISTS?
+    randomInt = random.randint(0,listSize)
     return randomInt
 
-# TODO(3) Define function that uses our random int as an index in each list, and prints results.
-def GenerateRandomCar():
-    print("Yay")
+
+# NOTDONE(3) Define function that uses our random int as an index in each list, and prints results.
 
 
-GenerateRandomCar()
+# TODO(5) Define a function that requires one PARAMETER. 
+# You'll pass in your dictionary when it's called. 
+
+# TODO(6) Your function will print random values from of each list.
+# HINT: Start by printing a random value of one list.
+
+# TODO(7) Add more values to some of your lists, so they're unequal.
+# Now, it shouldn't matter how many lists you have, or how many values each list has. 
+
+print("yay ☺")
