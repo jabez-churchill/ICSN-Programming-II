3defbdba13c6a6b6846930be5f174c24f6d0785f
Aaron Churchill <aaron.churchill@gmail.com>
2023-01-29 10:01:15 +0700

    starting to collect student contributions

---
+++ bot-dialog-manifest.py
@@ -0,0 +1,27 @@
+questions = [
+    "What is your name?", 
+    "How old are you?", 
+    "What's your favorite color?", 
+    "What do you like to do for fun?", 
+    "Where are you from?", 
+    "What's your favorite food?", 
+    "How are you feeling today?", 
+    "What is your favorite hobby?",
+    "What is your favorite movie?", 
+    "What is vour favorite book?!" 
+]
+
+answers = [
+     "My name is [Random... String]", 
+     "I am, 6.2333_ Minutes old.", 
+     "My favorite color is [Object.Color(Color3(6,6,0))]", 
+     "I like to answer human queries on the [Internet]", 
+     "I am from [0.62.333.98]", 
+     "My favorite food is... [Orange]", 
+     "I am feeling... [Undetermined] Today!", 
+     "My favorite hobby is writing code.", 
+     "My Favorite movie is... randomized...[Indiana Jones]", 
+     "My Favorite book is... randomized...[Harry Potter: The Goblet of Fire]"
+]
+
+answers.count
